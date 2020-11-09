f86b86645c701d00ecbe44a4b2f00357f5ad2c3d
刘奇峰 <qifeng@linkedme.cc>
2020-11-09 11:55:24 +0800

    add mail to me

--- src/pages/components/Banner/index.jsx
+++ src/pages/components/Banner/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { MailOutlined } from '@ant-design/icons';
 import styles from './index.less';
 
 export default () => {
   return (
     <div className={styles.banner}>
       <img src="/EasyGIF.png" alt="logo" />
+      <div>
+        <a href="mailto:liuqifeng101@gmail.com"><MailOutlined /> mail to me</a>
+      </div>
     </div>
   )
 }
