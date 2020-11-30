985bb4ae41bfc7be7799cd9851e5da4cd51d4633
刘奇峰 <qifeng@linkedme.cc>
2020-11-30 15:24:25 +0800

    add slack

--- src/pages/components/Banner/index.jsx
+++ src/pages/components/Banner/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { TwitterOutlined } from '@ant-design/icons';
+import { TwitterOutlined, SlackOutlined } from '@ant-design/icons';
 import styles from './index.less';
 
 export default () => {
   return (
     <div className={styles.banner}>
       <img src="/EasyGIF.png" alt="logo" />
-      <div>
+      <div className={styles.links}>
         <a href="https://twitter.com/liuqifeng101" target="_blank">
           <TwitterOutlined />
+        </a>
+        <a href="https://join.slack.com/t/easygif/shared_invite/zt-jwlaonb4-lu4gFkbVBVZ6zn2nu2MrmA" target="_blank">
+          <SlackOutlined />
         </a>
       </div>
     </div>
